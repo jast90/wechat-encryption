1ded337bafe5e69573ab76839510205654a7ba60
zhangzhwien <zhangzhiwen@ubox.cn>
2017-04-01 11:07:09 +0800

    小程序解密PKCS7Padding

---
+++ src/main/java/me/jastz/wechat/wxa/vo/WxaUserInfo.java
@@ -0,0 +1,94 @@
+package me.jastz.wechat.wxa.vo;
+
+/**
+ * Created by zhiwen on 2017/3/17.
+ */
+public class WxaUserInfo {
+    private String openId;
+    private String nickName;
+    private int gender;
+    private String city;
+    private String province;
+    private String country;
+    private String avatarUrl;
+    private String unionId;
+
+    public String getOpenId() {
+        return openId;
+    }
+
+    public void setOpenId(String openId) {
+        this.openId = openId;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public String getUnionId() {
+        return unionId;
+    }
+
+    public void setUnionId(String unionId) {
+        this.unionId = unionId;
+    }
+
+    @Override
+    public String toString() {
+        return "WxaUserInfo{" +
+                "openId='" + openId + '\'' +
+                ", nickName='" + nickName + '\'' +
+                ", gender=" + gender +
+                ", city='" + city + '\'' +
+                ", province='" + province + '\'' +
+                ", country='" + country + '\'' +
+                ", avatarUrl='" + avatarUrl + '\'' +
+                ", unionId='" + unionId + '\'' +
+                '}';
+    }
+}
+
